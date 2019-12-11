6509020ca0c66866508d682e2a0e3ab2341f53f9
Nikhil Motiani <nmotiani@paypal.com>
2019-12-11 19:42:18 +0530

    feat(templateroute): added template routes

--- index.js
+++ index.js
@@ -5,13 +5,28 @@ const app = express();
 
 app.use(bodyParser.json());
 
+const templates = {
+
+};
+
 app.get('/', function (req, res, next) {
     const query = req.query;
     res.status(200).json({ code: "success" });
 });
 
-const port  =process.env.PORT || 3000
-app.listen(port, function(){
+app.post('/adroit/template/:name', function (req, res, next) {
+    templates[req.param.name] = req.body;
+    res.status(200).json({ code: "success" });
+})
+
+app.get('/adroit/template/:name', function (req, res, next) {
+    res.status(200).json(templates[req.param.name] || {});
+})
+
+
+
+const port = process.env.PORT || 3000
+app.listen(port, function () {
     console.log("listening on ", port);
 });
 app.on('error', console.error);
